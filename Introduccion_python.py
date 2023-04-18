6a3721403ffead55922e3a197e879ee24017c6cd
elospieconomics@gmail.com <directorgeneral@r2.com.co>
2023-04-17 21:11:49 -0500

    primer_commit

---
+++ Introduccion_python.py
@@ -0,0 +1,32 @@
+
+# Creación de una Variable entera
+var_entera = 5
+
+# Creación de una Variable texto
+var_texto = 'Sebastian'
+var_texto2 = "Ospina"
+
+# Creación de una Variable float
+var_decimal = 1.67
+
+# Creación de una Variable boolean
+var_bool = True
+
+mi_var = 345
+
+nombre_comleto = var_texto + var_texto2
+
+print('Esta es una llamada de print')
+
+lista1 = [var_entera,var_bool,6.54]
+print(lista1)
+
+lista1[0]
+lista1[1]
+lista1[2]
+
+lista1[1] = False
+
+print(lista1)
+
+lista1[2] = 10.82
