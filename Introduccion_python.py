b336aded5938f5d13ab2076751645d05f8386a62
elospieconomics@gmail.com <directorgeneral@r2.com.co>
2023-04-23 21:33:40 -0500

    segunda clase

--- Introduccion_python.py
+++ Introduccion_python.py
@@ -30,3 +30,63 @@ lista1[1] = False
 print(lista1)
 
 lista1[2] = 10.82
+
+###################### Diccionarios ##############
+
+mi_datos = {'nombre':'Sebastian','edad': 32, 'ciudad':'Medellín'}
+mi_datos['nombre']
+mi_datos['ciudad']
+
+mi_datos['ciudad'] = 'Bogotá'
+
+mi_datos['apellido_1'] = 'Ospina'
+
+###################### Dataframes ##############
+
+import pandas as pd
+
+mi_dataframe = pd.DataFrame(mi_datos, index = [0] )
+
+mi_dataframe['nombre']
+mi_dataframe['nombre'].iloc[0]
+
+######################## Ciclo for ###################
+
+lista_numeros = [1,2,3,4,5]
+
+for num in lista_numeros:
+    print('Este es el número que va a sumar ', num)
+    print('El resultado de la suma es ',num + 1)
+
+#Crear rango para incluirlo en el for
+
+for posicion in range(len(lista_numeros)):
+    print(lista_numeros[posicion])
+
+############### If else elif ######################
+
+for numero in lista_numeros:
+    if (numero == 2) or (numero == 4):
+        print('El numero es par')
+    else:
+        print('El número es impar')
+
+for numero in lista_numeros:
+    if numero == 2 :
+        print('El numero es par')
+    elif numero == 4:
+        print('El numero es par')
+    else:
+        print('El número es impar')
+
+############### Funciones ###########################
+
+def mi_maquina_de_sumar(numero_1, numero_2):
+    resultado = numero_1 + numero_2
+    print(resultado)
+
+mi_maquina_de_sumar(6,12)
+
+def mi_maquina_de_sumar2(numero_1, numero_2):
+    resultado = numero_1 + numero_2
+    return resultado
---
+++ conexión_mt5.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import MetaTrader5 as mt5
+
+nombre = 67043467
+clave = 'Genttly.2022'
+servidor = 'RoboForex-ECN'
+path = r'C:\Program Files\MetaTrader 5\terminal64.exe'
+
+mt5.initialize(login = nombre, password = clave, server = servidor, path = path)
+rates = mt5.copy_rates_from_pos('Boom 500 Index',mt5.TIMEFRAME_M1,0,60)
+tabla = pd.DataFrame(rates)
+tabla['time'] = pd.to_datetime(tabla['time'], unit = 's')
+
+
+
