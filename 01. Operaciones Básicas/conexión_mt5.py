e64a2eca7d4136a017b4e1adc4368b3537be1acf
ELOSPO <elospieconomics@gmail.com>
2024-11-06 18:07:20 -0500

    fix num timeframes

--- 01. Operaciones Básicas/conexión_mt5.py
+++ 01. Operaciones Básicas/conexión_mt5.py
@@ -12,7 +12,7 @@ mt5.initialize(login = nombre, password = clave, server = servidor, path = path)
 # https://www.mql5.com/es/docs/python_metatrader5
 
 # rates = mt5.copy_rates_range("USDJPY", mt5.TIMEFRAME_M5, '2021-03-31' ,'2015-03-31' ) 
-rates = mt5.copy_rates_from_pos('EURUSD',mt5.TIMEFRAME_M1,0,99999)
+rates = mt5.copy_rates_from_pos('EURUSD',mt5.TIMEFRAME_M1,0,9999)
 tabla = pd.DataFrame(rates)
 tabla['time'] = pd.to_datetime(tabla['time'], unit = 's')
 
